2a8d96f0d929627b7d47c8bedc70b50b80501f64
Cyprien023 <cyprienluzuy@gmail.com>
2025-03-11 15:12:16 +0100

    test branch

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -17,7 +17,7 @@ const Contact: React.FC = () => {
                 <p>
                     📧 <strong>Email : </strong>
                     <a href="mailto:cyprienluzuy@gmail.com" className="text-blue-600 hover:underline">
-                        cyprienluzuy@gmail.com test
+                        cyprienluzuy@gmail.com
                     </a>
                 </p>
                 <p>
